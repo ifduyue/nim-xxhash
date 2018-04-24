4efc3f7cfbeb4f95dcb075857373ada299484654
Yue Du <ifduyue@gmail.com>
2018-04-24 23:52:39 +0800

    remove destructor

--- src/xxhash.nim
+++ src/xxhash.nim
@@ -6,14 +6,14 @@ import xxhash/private/c
 proc xxh32*(input: string, seed: uint32 = 0): uint32 =
   xxh32(input.cstring, input.len, seed)
 
-type XXH32 = ref object of RootObj
+type XXH32* = ref object of RootObj
   state: ptr XXH32_state_t
   seed: uint32
 
 proc newXXH32*(seed: uint32): XXH32 =
-  var state = XXH32_createState()
-  discard XXH32_reset(state, seed)
-  XXH32(state: state, seed: seed)
+  new(result, proc(self: XXH32) = discard XXH32_freeState(self.state))
+  result.state = XXH32_createState()
+  discard XXH32_reset(result.state, seed)
 
 proc update*(x: XXH32, s: string) =
   discard XXH32_update(x.state, s.cstring, s.len)
@@ -24,24 +24,21 @@ proc digest*(x: XXH32): uint32 =
 proc reset*(x: XXH32) =
   discard XXH32_reset(x.state, x.seed)
 
-proc `=destroy`(x: XXH32) =
-  discard XXH32_freeState(x.state)
-  x.state = nil
-
 
 # xxh64
 
 proc xxh64*(input: string, seed: uint64 = 0): uint64 =
   xxh64(input.cstring, input.len, seed)
 
-type XXH64 = ref object of RootObj
+type XXH64* = ref object of RootObj
   state: ptr XXH64_state_t
   seed: uint64
 
 proc newXXH64*(seed: uint64): XXH64 =
-  var state = XXH64_createState()
-  discard XXH64_reset(state, seed)
-  XXH64(state: state, seed: seed)
+  new(result, proc(self: XXH64) = discard XXH64_freeState(self.state))
+  result.seed = seed
+  result.state = XXH64_createState()
+  discard XXH64_reset(result.state, seed)
 
 proc update*(x: XXH64, s: string) =
   discard XXH64_update(x.state, s.cstring, s.len)
@@ -52,6 +49,3 @@ proc digest*(x: XXH64): uint64 =
 proc reset*(x: XXH64) =
   discard XXH64_reset(x.state, x.seed)
 
-proc `=destroy`(x: XXH64) =
-  discard XXH64_freeState(x.state)
-  x.state = nil
